e32b1061bff8554fa0eb8a3e4c1df952a6591b9d
depdiller <stanislavoronov@yandex.ru>
2021-12-21 22:18:24 +0300

    Documentations for all classes

--- FileSystemLib/src/Permissions.h
+++ FileSystemLib/src/Permissions.h
@@ -1,31 +1,77 @@
-//
-// Created by stani on 01.12.2021.
-//
-
+/**
+ * \file
+ * \brief Заголовочный файл класса Прав Доступа
+ *
+ * Данный класс описывает поведение Прав Доступа
+ * */
 #ifndef FILESYSTEM_PERMISSIONS_H
 #define FILESYSTEM_PERMISSIONS_H
 #include <array>
 
 namespace System {
+    /**
+     * \brief вспомогательная структура для
+     * удобства проверки на определенную операцию (r w x)
+     */
     struct rwx {
     private:
-        bool read;
-        bool write;
-        bool execute;
+        bool read; ///< чтение
+        bool write; ///< изменение
+        bool execute; ///< открыть
     public:
+        /**
+         * \brief стандартный конструктор
+         * @param codePermission код для прав (4-r 2-w 1-x)
+         * \throw invalid_argument некорректный код прав
+         */
         explicit rwx(unsigned int codePermission = 6);
+        /**
+         * \brief проверка на чтение
+         * @return можно или нет
+         */
         bool isReadable() const { return read; }
+        /**
+         * \brief проверка на запись
+         * @return можно или нет
+         */
         bool isWritable() const { return write; }
+        /**
+         * \brief проверка на исполнение
+         * @return можно или нет
+         */
         bool isExecutable() const { return execute; }
-        // move constructor
     };
 
+    /**
+     * \author Voronov Stanislav
+     * \version 1.0
+     * \date декабрь 2021
+     *
+     * \brief Класс Права Доступ
+     *
+     *
+     * */
     class Permissions {
     private:
-        std::array<rwx, 2> uoPermissions;
+        std::array<rwx, 2> uoPermissions; ///< массив с правами доступа
     public:
+        /**
+         * \brief стандартный конструктор
+         * @param codePermissions права доступа для user & others
+         * \throw invalid_argument некорректный код для прав доступа
+         */
         explicit Permissions(unsigned int codePermissions = 66);
+        /**
+         * \brief сеттер для прав доступа
+         * @param codePermission код доступа
+         * \throw invalid_argument некорректный код для прав доступа
+         * @return ссылка на измененный объект
+         */
         Permissions& setPermissions(unsigned int codePermission);
+        /**
+         * \brief геттер для прав доступа
+         * @return массив из прав доступа для user & others
+         */
         std::array<rwx, 2> getPermissions() const { return uoPermissions; }
     };
 }
--- FileSystemLib/src/User.h
+++ FileSystemLib/src/User.h
@@ -1,34 +1,90 @@
-//
-// Created by stani on 24.11.2021.
-//
-
+/**
+ * \file
+ * \brief Заголовочный файл класса Пользователя
+ *
+ * Данный класс описывает поведение Пользователя
+ * */
 #ifndef FILESYSTEM_USER_H
 #define FILESYSTEM_USER_H
 #include "Dir.h"
 #include <iostream>
 
 namespace System {
+    /**
+     * \author Voronov Stanislav
+     * \version 1.0
+     * \date декабрь 2021
+     *
+     * \brief Класс Пользователя
+     *
+     *
+     */
     class User {
     private:
-        size_t userId;
-        std::string name;
-        size_t key;
+        size_t userId; ///< идентификатор пользователя
+        std::string name; ///< имя пользователя
+        size_t key; ///< ключ пользователя для шифрования файлов
     public:
-        // constructors
+        /**
+         * \brief конструктор принимающий только имя пользователя
+         * @param name имя пользователя
+         */
         User(const std::string& name);
+        /**
+         * \brief конструктор принимающий имя пользователя и ключ для шифрования
+         * @param name имя пользователя
+         * @param key ключ для шифрования
+         */
         User(const std::string& name, size_t key);
-        // setters
+        /**
+         * \brief сеттер для ключа
+         * @param newKey новый ключ
+         * @return ссылка на измененный объект
+         */
         User& setKey(size_t newKey);
+        /**
+         * \brief сеттер для имени пользователя
+         * @param newName новое имя пользователя
+         * @return ссылка на измененный объект
+         */
         User& setName(std::string newName);
-        // getters
+        /**
+         * \brief геттер для имени пользователя
+         * @return имя пользователя в формате строки
+         */
         std::string getName() const { return name; }
+        /**
+         * \brief геттер для идентификатора пользователя
+         * @return идентификатор пользователя
+         */
         size_t getUserId() const { return userId; }
+        /**
+         * \brief геттер для ключа шифрования пользователя
+         * @return ключ шифрования пользователя
+         */
         size_t getKey() const { return key; }
-        // operators
+        /**
+         * \brief перегруженный оператор равенства через объект класса
+         * и строку с именем
+         */
         bool operator==(const std::string &username) const;
+        /**
+         * \brief перегруженный оператор отрицания через объект класса
+         * и строку с именем
+         */
         bool operator!=(const std::string &username) const;
+        /**
+         * \brief перегруженный оператор сравнения через объекты класса
+         */
         bool operator<(const User&) const;
+        /**
+         * \brief перегруженный оператор сравнения через объект класса
+         * и строку с именем
+         */
         bool operator<(const std::string &username) const;
+        /**
+         * \brief перегруженный оператор присваивания
+         */
         User &operator=(const User&);
     };
 }
